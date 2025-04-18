2fa7321d92c6c7a4410936bbfde042cf1154360c
Jens Astrup <jensaiden@gmail.com>
2025-04-18 19:17:16 -0400

    Fix package reference, tidy up imports

--- demo/src/components/ui/tabs.tsx
+++ demo/src/components/ui/tabs.tsx
@@ -1,8 +1,6 @@
 'use client'
 
 import React, { useState, createContext, useContext } from 'react'
-import * as React from 'react'
-import * as TabsPrimitive from '@radix-ui/react-tabs'
 
 import { cn } from '../../utils/cn'
 
